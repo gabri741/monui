29c0043c9d905126ffce9a0e32caf08b939a90b4
Gabriel Martins <gabrielmartins.mca@gmail.com>
2025-11-26 16:46:47 -0300

    swagger

--- event-service/src/main.ts
+++ event-service/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors(); 
-    await app.listen(3002);
-    console.log('🚀 EVENT-SERVICE is running on http://localhost:3002');
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3002);
+  console.log('🚀 EVENT-SERVICE is running on http://localhost:3002');
 }
 bootstrap();
--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); 
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors();
   await app.listen(3003);
 }
 bootstrap();
